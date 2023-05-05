081a01bfffd1964decb21fe37233bc38d79210c7
Daniel R Gorgonha <rogergorgonha@gmail.com>
2023-05-04 21:18:35 -0300

    criando domain com cliente, adicionando referencias ao entityframework e mapeando domain como dbset

---
+++ AR.Domain/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AR.Domain
+{
+    public class Cliente
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O Campo {0} deve ter no máximo {1} caracteres")]
+        public int Nome { get; set; }
+        public int CPF { get; set; }
+        public int Idade { get; set; }
+    }
+}
